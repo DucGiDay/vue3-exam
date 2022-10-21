9e3760b3ff63bfec923f4262b01cbbb1cadf3607
TrungDuc <duc123zzmm@gmail.com>
2022-10-21 15:32:10 +0700

    sửa css cho trang Login

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -85,5 +85,55 @@ export default {
 </script>
 
 <style>
+.card {
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
+  max-width: 350px;
+  margin: auto;
+  text-align: center;
+  font-family: arial;
+  padding: 1rem 0;
+  position: relative;
+}
 
+
+.card button {
+  border: none;
+  outline: 0;
+  padding: 12px;
+  color: white;
+  background-color: red;
+  border-radius: 4px;
+  text-align: center;
+  cursor: pointer;
+  width: 70%;
+  font-size: 18px;
+}
+
+.card button:hover {
+  opacity: 0.7;
+}
+
+label {
+  position: absolute;
+  left: 13%;
+  /* right: inherit; */
+}
+
+.card input {
+  border: 2px solid #eee;
+  width: 70%;
+  border-radius: 4px;
+  padding: .6em .6em;
+  margin: .5rem 0 1.5rem;
+}
+
+.noti {
+  color: red;
+  font-size: small;
+}
+
+.routerLink {
+  color: red;
+  cursor: pointer;
+}
 </style>
